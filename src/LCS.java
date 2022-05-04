67f1429a5512da24eb0cfc70afbd05fb3ca3322b
gyaltsentenzin <40735388+gyaltsentenzin@users.noreply.github.com>
2022-05-03 21:46:15 -0500

    Need final testing for arbitrary result

--- src/LCS.java
+++ src/LCS.java
@@ -3,50 +3,63 @@ import java.util.*;
 public class LCS {
 
     public String LongCommSub (char[] first, char[] second) {
-        String returnString [][] = new String[first.length+1][second.length+1];
+        String [][] comparisonMatrix = new String[second.length+1][first.length+1];
+        int rowLength = comparisonMatrix.length;
+        int colLength = comparisonMatrix[0].length;
 
-        for (int i = 0; i < returnString.length; i++) {
-            for(int j = 0; i < returnString[0].length; j++) {
-                returnString[i][j] = " ";
+        for (int i = 0; i < rowLength; i++) {
+            for(int j = 0; j < colLength; j++) {
+                comparisonMatrix[i][j] = "";
             }
         }
         
-        for (int i = 1; i < returnString.length; i++) {
-            for(int j = 1; i < returnString[0].length; j++) {
-                if(first[i] == second[j]){
-                    returnString[i][j] = returnString[i-1][j-1] + first[i];
+        for (int iRow = 1; iRow < rowLength; iRow++) {
+            for(int jCol = 1; jCol < colLength; jCol++) {
+                if(first[jCol-1] == second[iRow-1]) {
+                    comparisonMatrix[iRow][jCol] = comparisonMatrix[iRow-1][jCol-1] + first[iRow];
                 }
                 else {
-                    int l1 = returnString[i-1][j].length();
-                    int l2 = returnString[i-1][j-1].length();
-                    int l3 = returnString[i-1][j].length();
+                    int topNeighbor = comparisonMatrix[iRow-1][jCol].length();
+                    int DiagNeighbor = comparisonMatrix[iRow-1][jCol-1].length();
+                    int leftNeighbor = comparisonMatrix[iRow][jCol-1].length();
 
-                    if(l1 >= l2 && l1 >= l3){
-                        returnString[i][j] = returnString[i][j] + returnString[i-1][j];
+                    if(topNeighbor >= DiagNeighbor && topNeighbor >= leftNeighbor){
+                        comparisonMatrix[iRow][jCol] = comparisonMatrix[iRow-1][jCol];
                     } 
-                    else if (l2 >= l1 && l2 >= l3) {
-                        returnString[i][j] = returnString[i][j] + returnString[i-1][j-1];
+                    else if (DiagNeighbor >= topNeighbor && DiagNeighbor >= leftNeighbor) {
+                        comparisonMatrix[iRow][jCol] =  comparisonMatrix[iRow-1][jCol-1];
                     }
                     else {
-                        returnString[i][j] = returnString[i][j] + returnString[i-1][j];
+                        comparisonMatrix[iRow][jCol] = comparisonMatrix[iRow][jCol-1];
                     }
                 }
             }
         }
 
-        return returnString[first.length][second.length];
+        // Printing 2D array to compare the results
+        // for (int i = 0; i < rowLength; i++) {
+        //     for(int j = 0; j < colLength; j++) {
+        //         System.out.print(comparisonMatrix[i][j] + " "); 
+        //     }
+        //     System.out.println();
+        // }
+
+        // LCS final string
+        return comparisonMatrix[second.length][first.length];
     }
 
     public static void main(String[] args) throws Exception {
         LCS lcs = new LCS();
-        String s1 = "BANANA";
-        String s2 = "ATANA";
+        // String s1 = "BANANA";
+        // String s2 = "ATANA";
+
+        String s1 = "cgtcgttatccgtcactggttgagatcgagtaagcaagacg";
+        String s2 = "caaccatcgagtttgacgataagccgcaccagcgcgacat";
 
         char[] first = s1.toCharArray();
         char[] second = s2.toCharArray();
 
         System.out.println(lcs.LongCommSub(first, second));
-
 
     }
 }
